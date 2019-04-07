3109eab3e8cd18cabab2745f023a5fef0349b214
SerhiiFilipchuk <47062741+SerhiiFilipchuk@users.noreply.github.com>
2019-04-08 01:21:33 +0300

    updt

--- dist/js/all.js
+++ dist/js/all.js
@@ -3014,6 +3014,16 @@
 }));
 
 $(document).ready(function () {
-  
+
+  $('.sample1').mouseover(function() {
+    $('.sample-description').slideDown(500);  
+    $('.sample-shadow').fadeIn(500);  
+  });
+
+  $('.sample1').mouseout(function() {
+    $('.sample-description').slideUp(500);  
+    $('.sample-shadow').fadeOut(500);  
+  });
+
 });
 
