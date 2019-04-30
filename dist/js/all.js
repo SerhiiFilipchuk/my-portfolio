6c0256ca5a08c64d321197d3ed57c76c0adc5f7e
SerhiiFilipchuk <47062741+SerhiiFilipchuk@users.noreply.github.com>
2019-04-30 13:37:39 +0300

    update

--- dist/js/all.js
+++ dist/js/all.js
@@ -3015,15 +3015,5 @@
 
 $(document).ready(function () {
 
-  $('.sample1').mouseover(function() {
-    $('.sample-description').slideDown(500);  
-    $('.sample-shadow').fadeIn(500);  
-  });
-
-  $('.sample1').mouseout(function() {
-    $('.sample-description').slideUp(500);  
-    $('.sample-shadow').fadeOut(500);  
-  });
-
 });
 
--- js/app.js
+++ js/app.js
@@ -1,14 +1,4 @@
 $(document).ready(function () {
 
-  $('.sample1').mouseover(function() {
-    $('.sample-description').slideDown(500);  
-    $('.sample-shadow').fadeIn(500);  
-  });
-
-  $('.sample1').mouseout(function() {
-    $('.sample-description').slideUp(500);  
-    $('.sample-shadow').fadeOut(500);  
-  });
-
 });
 
